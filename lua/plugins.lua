6f56c0b4094022af708bb7b7de24beabe6c71f80
Chris B. Mueller <chris@cbmueller.de>
2023-08-11 19:08:28 +0200

    better plugin organization

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,28 +1,35 @@
 require("plugins_init")
 
 return require("packer").startup(function(use)
+    -- Visuals
     use("tjdevries/colorbuddy.nvim")
     use("lalitmee/cobalt2.nvim")
+    use("nvim-lualine/lualine.nvim")
 
+    -- Icons
     use("ryanoasis/vim-devicons")
-
     use("kyazdani42/nvim-web-devicons")
+
+    -- File Exploring
     use("kyazdani42/nvim-tree.lua")
-
-    use("ggandor/leap.nvim")
-
     use("natecraddock/workspaces.nvim")
-
     use("mhinz/vim-startify")
 
+    -- Navigation
+    use("ggandor/leap.nvim")
     use("romgrk/barbar.nvim")
-    use("nvim-lualine/lualine.nvim")
-    use("RRethy/vim-illuminate")
 
-    use("folke/which-key.nvim")
-
-    use("nvim-lua/plenary.nvim")
+    -- LSP
+    use("neovim/nvim-lspconfig")
     use("jose-elias-alvarez/null-ls.nvim")
+    use({
+        "nvim-treesitter/nvim-treesitter",
+        version = "0.9.0",
+        run = ":TSUpdate",
+    })
+
+    -- Help and Discovery Tools
+    use("folke/which-key.nvim")
     use({
         "nvim-telescope/telescope.nvim",
         tag = "0.1.0",
@@ -30,22 +37,12 @@ return require("packer").startup(function(use)
     use({ "nvim-telescope/telescope-ui-select.nvim" })
     use({ "mrjones2014/legendary.nvim" })
 
-    use("neovim/nvim-lspconfig")
+    -- Language Specific
     use("simrat39/rust-tools.nvim")
-
-    use({
-        "nvim-treesitter/nvim-treesitter",
-        version = "0.9.0",
-        run = ":TSUpdate",
-    })
 
     -- Completion framework:
     use("hrsh7th/nvim-cmp")
-
-    -- LSP completion source:
     use("hrsh7th/cmp-nvim-lsp")
-
-    -- Useful completion sources:
     use("hrsh7th/cmp-nvim-lua")
     use("hrsh7th/cmp-nvim-lsp-signature-help")
     use("hrsh7th/cmp-vsnip")
@@ -53,6 +50,8 @@ return require("packer").startup(function(use)
     use("hrsh7th/cmp-buffer")
     use("hrsh7th/vim-vsnip")
 
+    -- Others
+    use("nvim-lua/plenary.nvim")
     use({
         "neoclide/coc.nvim",
         branch = "release",
